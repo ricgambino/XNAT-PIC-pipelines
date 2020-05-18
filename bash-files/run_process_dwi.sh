1f213928c5738e0d66dc737281a312b99f9c50bc
szullino <sara.zullino@unito.it>
2020-05-18 11:45:33 +0200

    run_process_dwi.sh added

---
+++ bash-files/run_process_dwi.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+
+if [ "$1" == "--version" ]; then
+    echo 1
+    exit 0
+fi
+
+input=$1
+output=$2
+dir=pwd
+
+
+echo $input > "/home/xnat/input.txt"
+echo $output > "/home/xnat/output.txt"
+#/usr/local/MATLAB/R2015B/bin/matlab
+
+/usr/local/bin/matlab -nosplash -nodisplay << END || die "Matlab processing failed" 
+addpath(genpath('/home/xnat/pipeline/xnat-pipeline-engine/templates/scripts/process_DWI')); 
+addpath(genpath('/home/xnat/Documents/Executables/Matlab-files'));
+process_DWI('$input','$output') 
+exit; 
+END
+
+echo "Done"
+
