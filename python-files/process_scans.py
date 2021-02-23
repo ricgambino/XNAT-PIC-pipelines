eb05c229e7dba2be9aa5af093823256972c6eedf
szullino <sara.zullino@unito.it>
2021-02-23 10:52:35 +0100

    process_scans.py, matlab dir changed

--- python-files/process_scans.py
+++ python-files/process_scans.py
@@ -14,7 +14,8 @@ import matlab.engine
 def process_scans(download_info, path):
     eng = matlab.engine.start_matlab()
     for j in range(0, len(download_info)):
-        for scan_dirs in download_info[j]["Scan Dirs"]:
+        print(download_info)
+        for scan_dirs in download_info[j]["Scan Dirs"]:           
             if not os.path.exists(download_info[j]["Subject Folder"]):
                 os.mkdir(download_info[j]["Subject Folder"])
             if not os.path.exists(
@@ -46,7 +47,7 @@ def process_scans(download_info, path):
                 # print("Sono in process scan, folder is "+scan_dirs)
                 # print("output folder is "+download_info[j]["Subject Folder"]+'/'+download_info[j]['Session ID'])
                 # print(download_info[j]["Subject Folder"]+'/'+download_info[j]['Session ID'])
-                eng.cd("/home/xnat/Documents/XNAT-PIC/pipelines/stable/CIM-XNAT-pipelines/matlab-files", nargout=0)
+                eng.cd("/home/xnat/Documents/XNAT-PIC/XNAT-PIC-pipelines/stable/matlab-files", nargout=0)
                 # print(eng.ls())
 	        print(download_info[j]["Scan Type"])
 		if download_info[j]["Scan Type"] == ['EPI diffusion map']:
@@ -59,7 +60,7 @@ def process_scans(download_info, path):
                     + download_info[j]["Scan ID"],
                     nargout=0,
                 )
-		elif download_info[j]["Scan Type"] == ['MSME-T2-map in vivo']:
+		elif download_info[j]["Scan Type"] == ['MSME-T2-map in vivo short'] or download_info[j]["Scan Type"] == ['MSME-T2-map in vivo']:
                     eng.process_T2w(
                     scan_dirs,
                     download_info[j]["Subject Folder"]
